06a53422aecc69005a9a514a2361fe34ef425757
Tenk Gergely <tenk@lab104-18.iit.uni-miskolc.hu>
2018-10-19 08:50:46 +0200

    js

---
+++ js/index.js
@@ -0,0 +1,29 @@
+//alert("hai");
+
+function init(){
+//    document.getElementById("menuBar").style.fontSize="35px";
+//    document.getElementById("menuBar").style.display="none";
+    alert(add(5,6));
+   var cars = ["opel","toyota","ford"];
+    alert(cars[1]);
+    var person = {name: "en",age:22};
+    alert(person.name);
+    str="";
+    var numbers = [5,4,3,2,1,5];
+    numbers.forEach(asd);
+    alert(str);
+}
+
+function asd(value, index,array){
+    str=str+value;
+}
+
+function add(x,y){
+    return x+y;
+}
+
+function buttonHandler(){
+      alert("onclicknho");
+      alert(5 === "5" && 6 == 7);
+}
+
